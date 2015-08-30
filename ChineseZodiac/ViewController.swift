3b90fafcc3838d2c9d8343d7730bfe3c349966f7
jason <aizhaoyu@163.com>
2015-08-30 22:58:56 +0800

    I think it's OK

--- ChineseZodiac/ViewController.swift
+++ ChineseZodiac/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var year: UITextField!
+    @IBOutlet weak var zodiacImage: UIImageView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,21 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func calculateZodiac(sender: AnyObject){
+        println(year.text)
+        
+        var inputYear = year.text.toInt()
+        var basicYear = inputYear! % 12 - 3
+        if basicYear < 1
+        {
+            basicYear += 12
+        }
+        
+        var zodiacImageName = "\(basicYear)"
+        println("zodiacImageName:\(zodiacImageName)")
+
+        zodiacImage.image = UIImage(named: zodiacImageName)
+    }
 
 }
 
